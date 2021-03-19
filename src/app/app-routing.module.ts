e2883226b97dfaa8891b3197cdd28d679e5836a1
Andriatsirofo Sandy <sandyhenrinjaka22@gmail.com>
2021-03-19 15:49:32 +0300

    register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [{
-  path:'',component:LoginComponent
-}];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path:'login',component:LoginComponent },
+  { path:'register',component:RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
